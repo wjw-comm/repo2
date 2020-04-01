b5b045e7f7587816949749459971d395a74494b1
wangjunwen <1973128309@qq.com>
2020-04-01 16:45:34 +0800

    提交代码

---
+++ Day01_eesy_springmvc/src/main/java/cn/itcast/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.itcast.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(path = "/hello")
+    public String sayHello(){
+        System.out.println("hello,stringmvc");
+        return "success";
+    }
+}
